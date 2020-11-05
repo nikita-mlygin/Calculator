13649592c35ac7d16949835456cc2a8bcfccf252
Никита Малыгин <nikita.malygin2017@mail.ru>
2020-11-05 20:22:13 +0300

    комменты

--- Calculator XD/Source.cpp
+++ Calculator XD/Source.cpp
@@ -8,14 +8,15 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	float num1, num2;
-	char sign;
+	float num1, num2; // инициализируем 2 переменные в памяти для чисел, над которыми будем
+					  // производить вычисления
+	char sign; // и переменную для знака
 
-	cin >> num1 >> sign >> num2;
+	cin >> num1 >> sign >> num2; // получаем значения всех переменных
 
-	switch (sign) {
-	case '+':
-		cout << num1 << " + " << num2 << " = " << num1 + num2;
+	switch (sign) { // конструкция switch принимает значение переменной sign
+	case '+': // и сравнивает с каждым значением case
+		cout << num1 << " + " << num2 << " = " << num1 + num2; // и выполняет тело каждого case-а
 		break;
 	case '-':
 		cout << num1 << " - " << num2 << " = " << num1 - num2;
